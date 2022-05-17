f32043ef1dc0130e06c72fcb288bc1935e793081
chathu_08 <chathu.rash97@gmail.com>
2022-05-17 07:26:39 +0530

    Set up the left card

--- digital-business-card/src/leftcard/LeftCard.jsx
+++ digital-business-card/src/leftcard/LeftCard.jsx
@@ -4,7 +4,23 @@ import './LeftCard.css'
 
 const LeftCard = () => {
   return (
-    <div>LeftCard</div>
+    <div className="left__box">
+        <div className="left__card">
+            <img src="" alt="" className="left__img-style" />
+
+            <div className="left__detail-section">
+                <div className="left__intro-section"></div>
+
+                <div className="left__btn-container"></div>
+
+                <div className="left__about-section"></div>
+
+                <div className="left__interests-section"></div>
+            </div>
+
+            <div className="left__socialmedia-icons"></div>
+        </div>
+    </div>
   )
 }
 
