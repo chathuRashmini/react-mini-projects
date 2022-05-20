1323e33e480a4e2c5ebe583a709dac69ca9e8d32
chathu_08 <chathu.rash97@gmail.com>
2022-05-20 17:30:09 +0530

    Completed the new post submit functionality

--- reactjs-blog/src/App.js
+++ reactjs-blog/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
-import {Route, Routes, useHistory} from 'react-router-dom';
+import {Route, Routes, useNavigate} from 'react-router-dom';
+import { format } from 'date-fns'
 
 import About from './About'
 import Footer from './Footer'
@@ -11,16 +12,79 @@ import NewPost from './NewPost'
 import PostPage from './PostPage'
 
 function App() {
+
+  const [ search, setSearch ] = useState('')
+  const [ searchResults, setSearchResults ] = useState([])
+  const [ postTitle, setPostTitle ] = useState('')
+  const [ postBody, setPostBody ] = useState('')
+  const [ posts, setPosts ] = useState([
+    {
+      id: 1,
+      title: "My First Blog",
+      datetime: "July 01, 2021 11:17:12 AM",
+      body: "This is my first article for you to go through."
+    },
+    {
+      id: 2,
+      title: "My Second Blog",
+      datetime: "July 01, 2021 11:17:12 AM",
+      body: "This is my first article for you to go through."
+    },
+    {
+      id: 3,
+      title: "My Third Blog",
+      datetime: "July 01, 2021 11:17:12 AM",
+      body: "This is my first article for you to go through."
+    },
+  ])
+
+  const navigate = useNavigate()
+
+  const handleDelete = (id) => {
+    const postList = posts.filter(post => post.id !== id)
+    setPosts(postList)
+    navigate.push('/')
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    const id = posts.length ? posts[posts.length-1].id + 1 : 1
+    const datetime = format(new Date(), 'MMMM dd yyyy pp')
+    const newPost = { id, title: postTitle, datetime, body: postBody}
+    const allPosts = [...posts, newPost]
+
+    setPosts(allPosts)
+    setPostTitle('')
+    setPostBody('')
+    navigate.push('/')
+  }
+  
   return (
     <div className="App">
-      <Header />
-      <Navbar />
+      <Header title = "React JS Blog" />
+      <Navbar 
+        search={search}
+        setSearch={setSearch}
+      />
       
       <Routes>
-        <Route exact path="/" element={<Home />}/>
-        <Route path="/post" element={<NewPost />}/>
-        <Route path="/post/:id" element={<PostPage />}/>
+        <Route exact path="/" element={<Home posts={posts} />}/>
+        
+        <Route path="/post" element={
+          <NewPost 
+            handleSubmit = {handleSubmit}
+            postTitle = {postTitle}
+            setPostTitle = {setPostTitle}
+            postBody = {postBody}
+            setPostBody = {setPostBody}
+          />
+        } />
+
+        <Route path="/post/:id" element={<PostPage posts={posts} handleDelete={handleDelete} />}/>
+        
         <Route path="/about" element={<About />}/>
+        
         <Route path="*" element={<Missing />}/>
       </Routes>
 
--- reactjs-blog/src/NewPost.jsx
+++ reactjs-blog/src/NewPost.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
-const NewPost = () => {
+const NewPost = ({ handleSubmit, postTitle, setPostTitle, postBody, setPostBody }) => {
+    
     return (
-        <main>
-            <h1>New Post</h1>
+        <main className='NewPost'>
+            <h2>New Post</h2>
+
+            <form className="newPostForm" onSubmit={handleSubmit}>
+                <label htmlFor="postTitle">Title:</label>
+
+                <input 
+                    type="text" 
+                    id='postTitle'
+                    required
+                    value={postTitle}
+                    onChange={(e) => setPostTitle(e.target.value)}
+                />
+
+                <label htmlFor="postBody">Post:</label>
+
+                <textarea 
+                    id="postBody" 
+                    required
+                    value={postBody}
+                    onChange={(e) => setPostBody(e.target.value)}
+                    cols="30" 
+                    rows="10"
+                />
+
+                <button type='submit'>Submit</button>
+            </form>
         </main>
     )
 }
