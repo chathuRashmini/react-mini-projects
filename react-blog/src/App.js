cac8d4a95dc8c827d2ed364f736f976534741e3b
chathu_08 <chathu.rash97@gmail.com>
2022-05-20 07:49:04 +0530

    Set up the react-blog project

---
+++ react-blog/src/App.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+import { Route, Switch, useHistory } from 'react-router-dom'
+
+import About from './About'
+import Header from './Header'
+import Home from './Home'
+import Missing from './Missing'
+import Navbar from './Navbar'
+import NewPost from './NewPost'
+import PostPage from './PostPage'
+
+function App() {
+  return (
+    <div className="App">
+      Hi
+    </div>
+  );
+}
+
+export default App;
