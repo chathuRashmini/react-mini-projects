745cfa7e89c83a0c1de5c0ac812ef1c9a6829427
chathu_08 <chathu.rash97@gmail.com>
2022-05-20 07:14:19 +0530

    Completed the fetch-data project

--- fetch-data/src/App.js
+++ fetch-data/src/App.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
 
 import Form from './Form'
-import List from './List'
+// import List from './List'
+import Table from './Table'
 
 function App() {
 
@@ -33,7 +34,11 @@ function App() {
         setReqType = {setReqType}
       />
 
-      <List
+      {/* <List
+        items = {items}
+      /> */}
+
+      <Table
         items = {items}
       />
     </div>
