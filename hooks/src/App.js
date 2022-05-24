bb6799a36ebd59ff4e3487acb6846aa2e7df97e5
chathu_08 <chathu.rash97@gmail.com>
2022-05-24 20:24:39 +0530

    Completed useContext hook

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,22 +1,28 @@
+import React, { createContext } from "react";
+
 import ClassCounterUseeffect from "./classComponents/ClassCounterUseeffect";
 import ClassCounterUsestate from "./classComponents/ClassCounterUsestate";
 import ClassIntervalUseeffect from "./classComponents/ClassIntervalUseeffect";
 import ClassMouseUseeffect from "./classComponents/ClassMouseUseeffect";
+import UsecontextCompA from "./functionalComponents/useContextHook/UsecontextCompA";
+import UsecontextCompD from "./functionalComponents/useContextHook/UsecontextCompD";
 import FetchPost from "./functionalComponents/useEffectHook/FetchPost";
 import FetchPosts from "./functionalComponents/useEffectHook/FetchPosts";
 import MouseContainer from "./functionalComponents/useEffectHook/MouseContainer";
 import UseEffectBasicCounter from "./functionalComponents/useEffectHook/UseEffectBasicCounter";
 import UseEffectInterval from "./functionalComponents/useEffectHook/UseEffectInterval";
-import UseEffectMouse from "./functionalComponents/useEffectHook/UseEffectMouse";
 import UseStateBasicCounter from "./functionalComponents/useStateHook/UseStateBasicCounter";
 import UseStateThreeFunctionsCounter from "./functionalComponents/useStateHook/UseStateThreeFunctionsCounter";
 import UseStateWithArray from "./functionalComponents/useStateHook/UseStateWithArray";
 import UseStateWithObject from "./functionalComponents/useStateHook/UseStateWithObject";
 
+export const UserContext = createContext()
+export const ChannelContext = createContext()
+
 function App() {
   return (
     <div className="App">
-      <div>
+      <div style={{ backgroundColor: "silver" }}>
         <h2>01. useState() Hook</h2>
 
         <div className="useState-section">
@@ -37,7 +43,7 @@ function App() {
         </div>
       </div>
 
-      <div>
+      <div style={{ backgroundColor: "gold" }}>
         <h2>02. useEffect() Hook</h2>
 
         <div className="useState-section">
@@ -45,10 +51,10 @@ function App() {
           <UseEffectBasicCounter />
         </div>
 
-        {/* <div className="useState-section">
+        <div className="useState-section">
           <ClassMouseUseeffect />
           <MouseContainer />
-        </div> */}
+        </div>
 
         <div className="useState-section">
           <ClassIntervalUseeffect />
@@ -61,6 +67,26 @@ function App() {
 
         <div className="useState-section">
           <FetchPost />
+        </div>
+      </div>
+
+      <div style={{ backgroundColor: "burlywood" }}>
+        <h2>03. useContext() Hook</h2>
+
+        <div className="useState-section">
+          <UserContext.Provider value={'Edward Cullen'}>
+            <ChannelContext.Provider value={'Biology'}>
+              <UsecontextCompA />
+            </ChannelContext.Provider>
+          </UserContext.Provider>
+        </div>
+
+        <div className="useState-section">
+          <UserContext.Provider value={'Edward Cullen'}>
+            <ChannelContext.Provider value={'Biology'}>
+              <UsecontextCompD />
+            </ChannelContext.Provider>
+          </UserContext.Provider>
         </div>
       </div>
     </div>
